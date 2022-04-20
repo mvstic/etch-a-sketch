49c935eaa4f1bf6a69fc6d0a0d4bdf2d08131296
mvstic <86735882+mvstic@users.noreply.github.com>
2022-04-20 16:12:55 -0500

    Add slider and eraseGrid function

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const gridContainer = document.querySelector(".grid-container");
 const resetBtn = document.querySelector("#reset");
-const smallGrid = document.querySelector("#small-grid");
-const mediumGrid = document.querySelector("#medium-grid");
-const largeGrid = document.querySelector("#large-grid");
+const slider = document.querySelector("#size-slider");
 
 function createGrid(squares) {
+    gridContainer.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
     for (let i = 0; i < squares * squares; i++) {
         const eachSquare = document.createElement("div");
-        gridContainer.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
-        gridContainer.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
+        eachSquare.classList.add("eachSquare");
         gridContainer.appendChild(eachSquare);
         eachSquare.addEventListener('mouseover', () => {
             eachSquare.style.backgroundColor = "black";
@@ -19,7 +18,18 @@ function createGrid(squares) {
     }
 }
 
+function eraseGrid() {
+    gridContainer.innerHTML = "";
+}
+
+slider.addEventListener('input', () => {
+    let squares = parseInt(document.getElementById('size-slider').value);
+    eraseGrid();
+    createGrid(squares);
+  });
+
 createGrid(16)
+
 
 
 
