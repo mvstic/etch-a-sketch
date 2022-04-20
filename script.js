e48a7d11a94f80bb53f0e2730082d13f2dd75f43
mvstic <86735882+mvstic@users.noreply.github.com>
2022-04-20 11:22:25 -0500

    Remove get dimensions function and clearGrid function

--- script.js
+++ script.js
@@ -4,28 +4,21 @@ const smallGrid = document.querySelector("#small-grid");
 const mediumGrid = document.querySelector("#medium-grid");
 const largeGrid = document.querySelector("#large-grid");
 
-function getGridDimensions() {
-    let dimension = prompt(`What size would you like your grid to be? Write down a single number between 1-100.`)
-    createGrid(dimension);
-}
-
 function createGrid(squares) {
     for (let i = 0; i < squares * squares; i++) {
-        let eachSquare = document.createElement("div");
-        eachSquare.style.backgroundColor = "white";
-        eachSquare.style.border = "thin solid grey";
+        const eachSquare = document.createElement("div");
         gridContainer.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
         gridContainer.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
         gridContainer.appendChild(eachSquare);
         eachSquare.addEventListener('mouseover', () => {
             eachSquare.style.backgroundColor = "black";
         })
-        function clearGrid() {
-            resetBtn.addEventListener('click', () => {
-                eachSquare.style.backgroundColor = "white";
-            })
-        }
-    clearGrid();
+        resetBtn.addEventListener('click', () => {
+            eachSquare.style.backgroundColor = "white";
+        })
     }
 }
+
+function 
+
 
