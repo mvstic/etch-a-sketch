3948cc763f04442f0daab06adecf8f9effc64ef9
mvstic <86735882+mvstic@users.noreply.github.com>
2022-04-20 10:35:58 -0500

    Remove slider, keep buttons

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const gridContainer = document.querySelector(".grid");
-const resetButton = document.querySelector("#reset");
-const sizeSlider = document.querySelector("#size-slider");
+const resetBtn = document.querySelector("#reset");
+const smallGrid = document.querySelector("#small-grid");
+const mediumGrid = document.querySelector("#medium-grid");
+const largeGrid = document.querySelector("#large-grid");
 
 function getGridDimensions() {
     let dimension = prompt(`What size would you like your grid to be? Write down a single number between 1-100.`)
@@ -10,25 +12,19 @@ function getGridDimensions() {
 function createGrid(squares) {
     for (let i = 0; i < squares * squares; i++) {
         let eachSquare = document.createElement("div");
-        eachSquare.style.backgroundColor = "black";
+        eachSquare.style.backgroundColor = "white";
+        eachSquare.style.border = "thin solid grey";
         gridContainer.style.gridTemplateRows = `repeat(${squares}, 1fr)`;
         gridContainer.style.gridTemplateColumns = `repeat(${squares}, 1fr)`;
         gridContainer.appendChild(eachSquare);
         eachSquare.addEventListener('mouseover', () => {
-            eachSquare.style.backgroundColor = "white";
+            eachSquare.style.backgroundColor = "black";
         })
         function clearGrid() {
-            resetButton.addEventListener('click', () => {
-                eachSquare.style.backgroundColor = "black";
+            resetBtn.addEventListener('click', () => {
+                eachSquare.style.backgroundColor = "white";
             })
         }
     clearGrid();
     }
 }
-
-function updateGridDimensions(squares) {
-    sizeSlider.innerHTML = `${squares} x ${squares}`
-    createGrid(squares);
-  }
-
-sizeSlider.onmousemove = (e) => updateGridDimensions(e.target.value)
